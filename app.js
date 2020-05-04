9ba4e29ad2770bee5fd52a88b1d2305e2eead93b
Maxim Palotti <41835828+MyHouseIsBig@users.noreply.github.com>
2020-05-04 13:03:39 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+particlesJS.load('header', '/js/particles/particlesjs-config.json', function() {
+  console.log('particles.js loaded - callback');
+});
