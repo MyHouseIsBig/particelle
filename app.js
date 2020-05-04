9d8a54f1b7188f435a277e969782b4985bc01a63
Maxim Palotti <41835828+MyHouseIsBig@users.noreply.github.com>
2020-05-04 13:14:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@
 /* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-particlesJS.load('top', '/js/particles/particlesjs-config.json', function() {
+particlesJS.load('nimbus-is-editor', '/js/particles/particlesjs-config.json', function() {
   console.log('particles.js loaded - callback');
 });
